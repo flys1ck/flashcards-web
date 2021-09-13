14f8aa1b7cee5cbcc6fa2a466ae8670e6ca7f8ed
Gregor Zweig <zwickzwackzwieback@gmx.de>
2021-09-14 00:05:25 +0200

    style: use lowercase component

--- src/components/AsideLink.vue
+++ src/components/AsideLink.vue
@@ -3,7 +3,7 @@
     active-class="after:absolute after:inset-y-0 after:right-0 after:w-1.5 after:bg-teal-500 relative after:rounded-l"
     class="hover:bg-gray-200 group flex items-center px-4 py-2.5 text-lg"
   >
-    <Component
+    <component
       :is="icon"
       v-if="icon"
       class="group-hover:text-teal-700 w-6 h-6 text-teal-600"
